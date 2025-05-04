875b8821de85177ee00851207c1104ea1e541c01
Maxzimmerman <115488694+Maxzimmerman@users.noreply.github.com>
2025-05-04 21:44:59 +1000

    gg

---
+++ ExpenseTrackerApp/Migrations/20250305043933_AdjustedTransaction.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTrackerApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdjustedTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "WalletId",
+                table: "transactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_transactions_WalletId",
+                table: "transactions",
+                column: "WalletId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_transactions_wallets_WalletId",
+                table: "transactions",
+                column: "WalletId",
+                principalTable: "wallets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_transactions_wallets_WalletId",
+                table: "transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_transactions_WalletId",
+                table: "transactions");
+
+            migrationBuilder.DropColumn(
+                name: "WalletId",
+                table: "transactions");
+        }
+    }
+}
